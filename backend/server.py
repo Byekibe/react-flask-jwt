cebdf71c6c12461ca25f00481ddca3fa08188dd7
Byekibe <kibetpeter95@gmail.com>
2023-07-15 04:47:40 +0300

    create and protect routes using jwt

--- backend/server.py
+++ backend/server.py
@@ -1,5 +1,6 @@
 from flask import Flask, request
 from models import create_connection
+from flask_cors import CORS
 from flask_jwt_extended import create_access_token
 from flask_jwt_extended import get_jwt_identity
 from flask_jwt_extended import jwt_required
@@ -10,6 +11,7 @@ import os
 
 MY_SECRET_KEY= os.getenv("MY_SECRET_KEY")
 app = Flask(__name__)
+CORS(app)
 
 conn = create_connection()
 cursor = conn.cursor()
@@ -22,13 +24,21 @@ jwt = JWTManager(app)
 def create_token():
     email = request.json.get("email")
     password = request.json.get("password")
+    print(email, password)
 
-    if email != "test" or password != "test":
+    access_token = ""
+
+    if email != "test@gmail.com" or password != "test":
         return { "msg": "Wrong credentials"}, 401
-    
-    access_token = create_access_token(identity=email)
+    else:
+        access_token = create_access_token(identity=email)
     
     return { "access_token": access_token }
+
+@app.route("/protected")
+@jwt_required
+def protected():
+    return { "msg": "Protected route" }
 
 if __name__=="__main__":
     app.run(port=7002, debug=True)
